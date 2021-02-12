bba451feef64ba2b6590df84bb27edd0e3074861
EvgeniyMay <yevgeniy.may@gmail.com>
2021-02-12 16:25:25 +0200

    add lcolization

---
+++ src/main/java/com/myLearning/timeIsMoney/filter/LocalizationFilter.java
@@ -0,0 +1,26 @@
+package com.myLearning.timeIsMoney.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(filterName = "LocalizationFilter", urlPatterns = { "/*" })
+public class LocalizationFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+
+//        if(((HttpServletRequest)request).getSession().getAttribute("lang") == null) {
+//            ((HttpServletRequest) request).getSession().setAttribute("lang", "en");
+//        }
+
+        if (request.getParameter("lang") != null) {
+            ((HttpServletRequest)request).getSession().setAttribute("lang", request.getParameter("lang"));
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
--- src/main/webapp/main.jsp
+++ src/main/webapp/main.jsp
@@ -1,9 +1,18 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<fmt:setLocale value="${sessionScope.lang}"/>
+<fmt:setBundle basename="localization.messages"/>
+
 <html>
 <head>
     <title>Main</title>
 </head>
 <body>
+    <nav>
+        <a href="?lang=en"><fmt:message key="language.en" /></a>
+        <a href="?lang=ru"><fmt:message key="language.ru" /></a>
+    </nav>
     <h1>Main page</h1>
     <a href="/app/activity">Activity</a>
     <a href="/app/user">User</a>
@@ -11,5 +20,6 @@
     <a href="/app/login">Login</a>
     <a href="/app/profile">My Profile</a>
     <a href="/app/logout">Logout</a>
+    <h2><fmt:message key="welcome" /></h2>
 </body>
 </html>
